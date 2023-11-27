d340d98447079955180b1eb79f365d806c105fc6
Raphael Schweizer <raphael.schweizer@bbv.ch>
2023-11-27 17:37:20 +0100

    fix API warnings

--- src/LambdaTale/Specifications.cs
+++ src/LambdaTale/Specifications.cs
@@ -4,8 +4,19 @@ using LambdaTale.Sdk;
 
 namespace LambdaTale;
 
+/// <summary>
+/// Provides access to step definition methods.
+/// </summary>
 public static class Specifications
 {
+    /// <summary>
+    /// Defines a step in the current scenario.
+    /// </summary>
+    /// <param name="text">The step text.</param>
+    /// <param name="body">The action that will perform the step.</param>
+    /// <returns>
+    /// An instance of <see cref="IStepBuilder"/>.
+    /// </returns>
     public static IStepDefinition Spec(string text, Func<Task> body)
     {
         var stepDefinition = new StepDefinition
@@ -18,6 +29,14 @@ public static class Specifications
         return stepDefinition;
     }
 
+    /// <summary>
+    /// Defines a step in the current scenario.
+    /// </summary>
+    /// <param name="text">The step text.</param>
+    /// <param name="body">The action that will perform the step.</param>
+    /// <returns>
+    /// An instance of <see cref="IStepBuilder"/>.
+    /// </returns>
     public static IStepDefinition Spec(string text, Action body)
     {
         var stepDefinition = new StepDefinition
